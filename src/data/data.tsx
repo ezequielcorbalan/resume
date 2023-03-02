a493fb87fa4b060e73e9adb3aa81b67e41458cc3
r-cbl <ezequiel@woowup.com>
2023-03-02 14:37:52 -0300

    init commit

---
+++ src/data/data.tsx
@@ -0,0 +1,347 @@
+import {
+  AcademicCapIcon,
+  CalendarIcon,
+  DownloadIcon,
+  FlagIcon,
+  MapIcon,
+  OfficeBuildingIcon,
+  SparklesIcon,
+} from '@heroicons/react/outline';
+
+import LinkedInIcon from '../components/Icon/LinkedInIcon';
+import heroImage from '../images/header-background.webp';
+import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
+import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
+import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
+import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
+import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
+import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
+import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
+import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
+import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
+import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
+import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
+import profilepic from '../images/profilepic.jpg';
+import testimonialImage from '../images/testimonial.webp';
+import {
+  About,
+  ContactSection,
+  ContactType,
+  Hero,
+  HomepageMeta,
+  PortfolioItem,
+  SkillGroup,
+  Social,
+  TestimonialSection,
+  TimelineItem,
+} from './dataDef';
+
+/**
+ * Page meta data
+ */
+export const homePageMeta: HomepageMeta = {
+  title: 'Ezequiel Corbalan Resume',
+  description: "This resume contains all experience i have in my Software Engineer career",
+};
+
+/**
+ * Section definition
+ */
+export const SectionId = {
+  Hero: 'hero',
+  About: 'about',
+  Contact: 'contact',
+  Portfolio: 'portfolio',
+  Resume: 'resume',
+  Skills: 'skills',
+  Stats: 'stats',
+  Testimonials: 'testimonials',
+} as const;
+
+export type SectionId = typeof SectionId[keyof typeof SectionId];
+
+/**
+ * Hero section
+ */
+export const heroData: Hero = {
+  imageSrc: heroImage,
+  name: `I'm Ezequiel Corbalan.`,
+  description: (
+    <>
+      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
+        I'm a <strong className="text-stone-100">Tech LEAD / Full Stack Software Engineer</strong>, currently working
+        at <strong className="text-stone-100">Woowup Inc.</strong> helping design/build features for CRM Retail.
+      </p>
+      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
+        I love teaching. Nowadays, I work as a <strong className="text-stone-100">technical high school professor</strong> in <strong className="text-stone-100">Buenos aires</strong>.
+        I strongly believe the next generation has the <strong className="text-stone-100">power</strong> to increase the quality of software.
+      </p>
+    </>
+  ),
+  actions: [
+    {
+      href: '/assets/resume.pdf',
+      text: 'Resume',
+      primary: true,
+      Icon: DownloadIcon,
+    },
+    {
+      href: `#${SectionId.Contact}`,
+      text: 'Contact',
+      primary: false,
+    },
+  ],
+};
+
+/**
+ * About section
+ */
+export const aboutData: About = {
+  profileImageSrc: profilepic,
+  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
+  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
+  in.`,
+  aboutItems: [
+    {label: 'Location', text: 'Buenos aires, Argentina', Icon: MapIcon},
+    {label: 'Age', text: '28', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Argentinian', Icon: FlagIcon},
+    {label: 'Interests', text: 'Motorcycles, Sports, Video Games', Icon: SparklesIcon},
+    {label: 'Study', text: 'Tecnical National University (UTN)', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'Woowup inc.', Icon: OfficeBuildingIcon},
+  ],
+};
+
+/**
+ * Skills section
+ */
+export const skills: SkillGroup[] = [
+  {
+    name: 'Spoken languages',
+    skills: [
+      {
+        name: 'English',
+        level: 7,
+      },
+      {
+        name: 'Spanish',
+        level: 10,
+      },
+    ],
+  },
+  {
+    name: 'Backend development',
+    skills: [
+      {
+        name: 'PHP',
+        level: 9,
+      },
+      {
+        name: 'node.js',
+        level: 8,
+      },
+      {
+        name: 'python',
+        level: 6,
+      },
+    ],
+  },
+  {
+    name: 'Frontend development',
+    skills: [
+      {
+        name: 'React',
+        level: 7,
+      },
+      {
+        name: 'ANGULAR',
+        level: 6,
+      }
+    ],
+  },
+  {
+    name: 'Arquitecture',
+    skills: [
+      {
+        name: 'AWS',
+        level: 8,
+      }
+    ],
+  },
+];
+
+/**
+ * Portfolio section
+ */
+export const portfolioItems: PortfolioItem[] = [
+  {
+    title: 'Project title 1',
+    description: 'Give a short description of your project here.',
+    url: 'https://timbaker.me',
+    image: porfolioImage1,
+  },
+  {
+    title: 'Project title 2',
+    description: 'Give a short description of your project here.',
+    url: 'https://timbaker.me',
+    image: porfolioImage2,
+  },
+  {
+    title: 'Project title 3',
+    description: 'Give a short description of your project here.',
+    url: 'https://timbaker.me',
+    image: porfolioImage3,
+  },
+  {
+    title: 'Project title 4',
+    description: 'Give a short description of your project here.',
+    url: 'https://timbaker.me',
+    image: porfolioImage4,
+  },
+  {
+    title: 'Project title 5',
+    description: 'Give a short description of your project here.',
+    url: 'https://timbaker.me',
+    image: porfolioImage5,
+  },
+  {
+    title: 'Project title 6',
+    description: 'Give a short description of your project here.',
+    url: 'https://timbaker.me',
+    image: porfolioImage6,
+  },
+  {
+    title: 'Project title 7',
+    description: 'Give a short description of your project here.',
+    url: 'https://timbaker.me',
+    image: porfolioImage7,
+  },
+  {
+    title: 'Project title 8',
+    description: 'Give a short description of your project here.',
+    url: 'https://timbaker.me',
+    image: porfolioImage8,
+  },
+  {
+    title: 'Project title 9',
+    description: 'Give a short description of your project here.',
+    url: 'https://timbaker.me',
+    image: porfolioImage9,
+  },
+  {
+    title: 'Project title 10',
+    description: 'Give a short description of your project here.',
+    url: 'https://timbaker.me',
+    image: porfolioImage10,
+  },
+  {
+    title: 'Project title 11',
+    description: 'Give a short description of your project here.',
+    url: 'https://timbaker.me',
+    image: porfolioImage11,
+  },
+];
+
+/**
+ * Resume section -- TODO: Standardize resume contact format or offer MDX
+ */
+export const education: TimelineItem[] = [
+  {
+    date: 'Actually',
+    location: 'Internet',
+    title: 'AWS Well-Architected',
+    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+  },
+  {
+    date: 'Actually',
+    location: 'Tecnical Nacional University, UTN',
+    title: 'systems engineering',
+    content: <p>Its the perfect combination to learn software and management. Has soft and hard skills to manage entire software lifetime.</p>,
+  },
+];
+
+export const experience: TimelineItem[] = [
+  {
+    date: 'March 2010 - Present',
+    location: 'Awesome Development Company',
+    title: 'Senior UX Engineer',
+    content: (
+      <p>
+        Describe work, special projects, notable achievements, what technologies you have been working with, and
+        anything else that would be useful for an employer to know.
+      </p>
+    ),
+  },
+  {
+    date: 'March 2007 - February 2010',
+    location: 'Garage Startup Studio',
+    title: 'Junior bug fixer',
+    content: (
+      <p>
+        Describe work, special projects, notable achievements, what technologies you have been working with, and
+        anything else that would be useful for an employer to know.
+      </p>
+    ),
+  },
+];
+
+/**
+ * Testimonial section
+ */
+export const testimonial: TestimonialSection = {
+  imageSrc: testimonialImage,
+  testimonials: [
+    {
+      name: 'John Doe',
+      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
+      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
+    },
+    {
+      name: 'Jane Doe',
+      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
+      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
+    },
+    {
+      name: 'Someone else',
+      text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
+      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
+    },
+  ],
+};
+
+/**
+ * Contact section
+ */
+
+export const contact: ContactSection = {
+  headerText: 'Get in touch.',
+  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  items: [
+    {
+      type: ContactType.Email,
+      text: 'reachout@timbaker.me',
+      href: 'mailto:reachout@timbaker.me',
+    },
+    {
+      type: ContactType.Location,
+      text: 'Victoria BC, Canada',
+      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+    },
+    {
+      type: ContactType.Instagram,
+      text: '@tbakerx',
+      href: 'https://www.instagram.com/tbakerx/',
+    },
+    {
+      type: ContactType.Github,
+      text: 'tbakerx',
+      href: 'https://github.com/tbakerx',
+    },
+  ],
+};
+
+/**
+ * Social items
+ */
+export const socialLinks: Social[] = [
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/ecorbalan/'}
+];
